e84fc27baa0a6d8c013f676bb20ce7b3d90713f5
Salomao Rodrigues <salomao88@gmail.com>
2022-12-03 16:25:56 +0000

    Read input file

--- 2022/02/src/main.rs
+++ 2022/02/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
+fn read_input_file() -> String {
+    let args: Vec<String> = env::args().collect();
+    let file_path: &String = &args[1];
+
+    fs::read_to_string(file_path).expect("Should have been able to read the file")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = read_input_file();
+
+    println!("Contents: {contents}");
 }
