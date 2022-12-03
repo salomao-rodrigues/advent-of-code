c3a5882154208cc88b306d4a76649a97fee6154a
Salomao Rodrigues <salomao88@gmail.com>
2022-12-03 17:56:38 +0000

    Convert XYZ inputs to ABC

--- 2022/02/src/main.rs
+++ 2022/02/src/main.rs
@@ -8,8 +8,31 @@ fn read_input_file() -> String {
     fs::read_to_string(file_path).expect("Should have been able to read the file")
 }
 
-fn main() {
-    let contents = read_input_file();
+fn convert_xyz_to_abc(c: char) -> char {
+    char::from_u32(c as u32 - 23).unwrap()
+}
 
-    println!("Contents: {contents}");
+fn calculate_score(rounds: String) -> u32 {
+    rounds.split("\n").fold(0, |acc, r| {
+        if r.is_empty() {
+            return acc;
+        }
+
+        let hands: Vec<char> = r
+            .replace(" ", "")
+            .chars()
+            .to_owned()
+            .map(convert_xyz_to_abc)
+            .collect();
+
+        println!("ASCII value: {}", hands[1]);
+        acc + 1
+    })
+}
+
+fn main() {
+    // would be more efficient to read line by line
+    let contents = read_input_file();
+    let score: u32 = calculate_score(contents);
+    println!("Score: {score}");
 }
